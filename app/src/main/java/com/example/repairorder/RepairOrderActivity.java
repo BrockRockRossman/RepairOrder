656c904ebc3440c47c5852dfffb427c75f1aee7f
brockrossman <brock.rossman@gmail.com>
2025-09-04 11:56:32 -0500

    added event listener for submit button

--- app/src/main/java/com/example/repairorder/RepairOrderActivity.java
+++ app/src/main/java/com/example/repairorder/RepairOrderActivity.java
@@ -1,7 +1,10 @@
 package com.example.repairorder;
 
+import android.hardware.biometrics.PromptContentItemPlainText;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -11,16 +14,56 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
 import java.util.Random;
 
 public class RepairOrderActivity extends AppCompatActivity {
 
     double numbers = 0.0;
+    TextView totalTV;
+
+    EditText A1PT;
+    EditText A2PT;
+    EditText A3PT;
+    EditText A4PT;
+    EditText A5PT;
+    EditText A6PT;
+
+
+
+
+
+    // Create button variable
+    Button submitB;
+    // clreate button listener
+    View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+
+            Random gen = new Random();
+            double number = gen.nextDouble();
+
+
+            String v1 = "$ " + R.string.Sub_TotalNum;
+            totalTV.setText(v1);
+
+            String value = totalTV.getText().toString();
+
+            Double subTotal = Double.parseDouble(A1PT.getText().toString());
+
+            String orderTypeValue = A1PT.getText().toString();
+
+
+        }
+
+    };
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
 
 
 
@@ -39,19 +82,28 @@ public class RepairOrderActivity extends AppCompatActivity {
 
 
 
-        TextView totalTV = findViewById(R.id.SubTotalNum);
+        A1PT = findViewById(R.id.plainTextA1);
+        A2PT = findViewById(R.id.plainTextA2);
+        A3PT = findViewById(R.id.plainTextA3);
+        A4PT = findViewById(R.id.plainTextA4);
+        A5PT = findViewById(R.id.plainTextA5);
+        A6PT = findViewById(R.id.plainTextA6);
 
-        Random gen = new Random();
+        totalTV = findViewById(R.id.SubTotalNum);
+        submitB = findViewById(R.id.submitButton);
+        submitB.setOnClickListener(buttonListener);
 
-        double number = gen.nextDouble();
 
 
-        String v1 = "$ " + R.string.Sub_TotalNum;
-        totalTV.setText(v1);
 
-        String value = totalTV.getText().toString();
 
-        Log.i("TEST", value);
+
+
+
+
+
+
+
 
     }
 }
